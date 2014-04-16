d4ce667f0e75102d60c6833716c6d0794fede2bd
Sahand Nayebaziz <snayebaz@uci.edu>
2014-04-16 11:32:59 -0700

    added basic floors, background

--- solo/mygame.py
+++ solo/mygame.py
@@ -1,5 +1,11 @@
+# Created by Sahand Nayebaziz
+# April 2014, UC Irvine
+#
+#
+
 # necessary imports
 import pygame as pg
+import random as rd
 
 # pg off the line
 pg.init()
@@ -7,37 +13,63 @@ pg.init()
 # the starting gun is loaded
 def play():
 
-	# pg_created_screen off the line with x_size and y_size
-	screen = pg.display.set_mode((1000, 420))
+	# display information
+	screen_width = 1000
+	screen_height = 420
 
-	# this is a real grown-up game honey, I think it's time it was given a real refresh rate
+	red = (255,0,0)
+	green = (0,255,0)
+	blue = (0,0,255)
+	darkBlue = (0,0,128)
+	white = (255,255,255)
+	black = (0,0,0)
+	pink = (255,200,200)
+
+	# create a screen off the line with x_size and y_size
+	screen = pg.display.set_mode((screen_width, screen_height))
+
+	# get ready for refresh rate
 	clock = pg.time.Clock()
 
 	# bring in player image
 	player_image = pg.image.load('player.png')
+	background_image = pg.image.load('bg.png')
 	player_x = 90
-	player_y = 300
+	player_y = 300 
+	floor_x = 1000
+	floor_y = 340
+
+	# speed of advancement
+	dx = 1.6
+
+	# empty list of floor lengths
+	floor_lengths = []
+
+	# floor counter
+	floor_counter = 0
 
 	# watch the world turn
 	while True:
-
+		
 		# ninety-nine percent sure this means 30 fps
-		clock.tick(30)
+		clock.tick(40)
+
+		# make a new floor
+		floor_lengths.append(rd.randint(200, 600))
 
 		# input listener
 		key = pg.key.get_pressed()
-		if key[pg.K_LEFT]:
-			player_x -= 10
-		if key[pg.K_RIGHT]:
-			player_x += 10
 		if key[pg.K_UP]:
-			player_y -= 20
+			player_y -= 9
 		if key[pg.K_DOWN]:
 			player_y += 10
 
 		# gravity mechanics	
 		if player_y != 300:
-			player_y += 10
+			player_y += 3
+
+		# move floors
+		floor_x -= dx
 
 		# quit mechanics	
 		for event in pg.event.get(): # the super listener
@@ -47,13 +79,23 @@ def play():
 				return # bail
 
 		# fill screen with a nice white... light..... so.... pretty.... 
-		screen.fill((241, 241, 241))
+		screen.blit(background_image, (0,0))
 
-		# blit our little guy onto the screen wherever we so please!
-		screen.blit(player_image, (player_x, player_y))
+		# draw player
+		pg.draw.rect(screen,blue,(player_x, player_y, 10, 40))
 
+		# draw a floor
+		pg.draw.rect(screen, red,(floor_x - 900, floor_y, 700, 20))
+		pg.draw.rect(screen, red,(floor_x, floor_y, 400, 20))
+		pg.draw.rect(screen, red,(floor_x + 500, floor_y, 400, 20))
+		pg.draw.rect(screen, red,(floor_x + 1000, floor_y, 400, 20))
+		pg.draw.rect(screen, red,(floor_x + 1500, floor_y, 400, 20))
+		
 		# let it snow, let it snow, let it snow
-		pg.display.flip()
+		pg.display.update()
+
+		# slowly speed up
+		dx = dx * 1.006
 
 # the starting gun
 play()
