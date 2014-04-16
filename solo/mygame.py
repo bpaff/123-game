140189a6eeac23872bbd158db157a1593e0b6dab
Sahand Nayebaziz <snayebaz@uci.edu>
2014-04-16 13:50:01 -0700

    floor generation now exists, but jumping is still crappy

--- solo/mygame.py
+++ solo/mygame.py
@@ -10,93 +10,87 @@ import random as rd
 # pg off the line
 pg.init()
 
+# cave floor class
+class Floor:
+	length = 0
+	height = 0
+	x = 0
+	y = 0
+	
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		
+
 # the starting gun is loaded
 def play():
-
-	# display information
 	screen_width = 1000
 	screen_height = 420
-
 	red = (255,0,0)
 	green = (0,255,0)
 	blue = (0,0,255)
-	darkBlue = (0,0,128)
 	white = (255,255,255)
 	black = (0,0,0)
-	pink = (255,200,200)
-
-	# create a screen off the line with x_size and y_size
-	screen = pg.display.set_mode((screen_width, screen_height))
-
-	# get ready for refresh rate
+	player_x = 90
+	player_y = 100
+	player_height = 40
+	all_floors = []
+	
 	clock = pg.time.Clock()
-
-	# bring in player image
 	player_image = pg.image.load('player.png')
 	background_image = pg.image.load('bg.png')
-	player_x = 90
-	player_y = 300 
-	floor_x = 1000
-	floor_y = 340
 
-	# speed of advancement
-	dx = 1.6
+	screen = pg.display.set_mode((screen_width, screen_height))
 
-	# empty list of floor lengths
-	floor_lengths = []
-
-	# floor counter
-	floor_counter = 0
-
-	# watch the world turn
 	while True:
+		clock.tick(48)
 		
-		# ninety-nine percent sure this means 30 fps
-		clock.tick(40)
-
-		# make a new floor
-		floor_lengths.append(rd.randint(200, 600))
-
-		# input listener
 		key = pg.key.get_pressed()
-		if key[pg.K_UP]:
-			player_y -= 9
-		if key[pg.K_DOWN]:
-			player_y += 10
-
-		# gravity mechanics	
-		if player_y != 300:
+		if key[pg.K_SPACE]:
+			player_y -= 5
+			
+		collide = False;
+			
+		for floor in all_floors:
+			if (floor.x < player_x < floor.x + floor.width):
+				if player_y < floor.y - player_height:
+					collide = False;
+				if player_y >= floor.y - player_height:
+					collide = True;
+					
+		if not collide:
 			player_y += 3
+			
 
-		# move floors
-		floor_x -= dx
+		if all_floors == []:
+			all_floors.append(Floor(0, 340, 805, 300))
+		else:
+			for floor in all_floors:
+				if floor.x + floor.width == 800:
+					all_floors.append(Floor(1000, rd.randint(200,400), 805, 400))
+				if floor.x + floor.width <= 0:
+					all_floors.remove(floor)
 
-		# quit mechanics	
-		for event in pg.event.get(): # the super listener
-			if event.type == pg.QUIT: # if the red button is pushed
-				return # bail
-			if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE: # if ESC is pressed
-				return # bail
+		screen.fill(white)
 
-		# fill screen with a nice white... light..... so.... pretty.... 
-		screen.blit(background_image, (0,0))
+		pg.draw.rect(screen, blue, (player_x, player_y, 10, player_height))
 
-		# draw player
-		pg.draw.rect(screen,blue,(player_x, player_y, 10, 40))
-
-		# draw a floor
-		pg.draw.rect(screen, red,(floor_x - 900, floor_y, 700, 20))
-		pg.draw.rect(screen, red,(floor_x, floor_y, 400, 20))
-		pg.draw.rect(screen, red,(floor_x + 500, floor_y, 400, 20))
-		pg.draw.rect(screen, red,(floor_x + 1000, floor_y, 400, 20))
-		pg.draw.rect(screen, red,(floor_x + 1500, floor_y, 400, 20))
+		for floor in all_floors:
+			floor.x -= 5
+			pg.draw.rect(screen, red, (floor.x, floor.y, floor.width, floor.height))
 		
-		# let it snow, let it snow, let it snow
 		pg.display.update()
+		
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				return
+			if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+				return 
+				
+		if player_y == screen_height:
+			return
 
-		# slowly speed up
-		dx = dx * 1.006
-
-# the starting gun
 play()
 	
