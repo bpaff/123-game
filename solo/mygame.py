275f237743ebf8b89326e70e5f91c681191ac30e
Sahand Nayebaziz <snayebaz@uci.edu>
2014-04-24 14:50:12 -0700

    rebuilding game in main/subroutine

--- solo/mygame.py
+++ solo/mygame.py
@@ -1,98 +1,107 @@
-# Created by Sahand Nayebaziz
-# April 2014, UC Irvine
+# BY SAHAND NAYEBAZIZ
+# UCI SPRING 2014
 #
 #
 
-# necessary imports
+from random import randint
 import pygame as pg
-import random as rd
 
-# pg off the line
+
+############################################################
+
+def process_input():
+	game_status = 1
+	jump = 0
+	for event in pg.event.get():
+		if event.type == pg.QUIT:
+			game_status = 0 # 0 for game over, 1 for play
+	key = pg.key.get_pressed()
+	if key[pg.K_UP]:
+		jump = 1
+	elif key[pg.K_ESCAPE]:
+		game_status = 0
+
+	return game_status, jump
+
+############################################################
+
+def draw_everything(screen, player, floors):
+    screen.fill((32, 32, 32))
+
+    pg.draw.rect(screen, (255, 255, 255), (player['x'], player['y'], player['width'], player['height']))
+
+    for key in floors:
+    	pg.draw.rect(screen, (90, 90, 90), (key, floors[key][0], floors[key][1], 600))
+    
+    pg.display.update()
+
+############################################################
+
+def create_player():
+	player = {'x': 20, 'y': 100, 'width': 7, 'height': 30}
+	return player
+
+############################################################
+
+def move_player(player, jump, jump_start, jump_time):
+	if jump:
+		if jump_time == 0:
+			jump_start = pg.time.get_ticks() + 1
+			player['y'] -= 3
+			jump_time = 1
+		if jump_time < 600:
+			player['y'] -= 2
+			jump_time = pg.time.get_ticks() - jump_start
+
+	if not jump:
+		jump_start = 0
+		jump_time = 0
+		player['y'] += 3
+
+	return player, jump_start, jump_time
+
+
+############################################################
+
+def create_floors():
+	floors = {}
+	return floors
+
+############################################################
+
+def move_floors(floors):
+	if len(floors) == 0:
+		floors = {0: (140, 100)}
+	if len(floors) != 0:
+		for key in floors:
+			floors[key - 1] = (floors[key][0], floors[key][1])
+			del floors[key]
+	return floors
+
+############################################################
+
 pg.init()
+clock = pg.time.Clock()
 
-# cave floor class
-class Floor:
-	length = 0
-	height = 0
-	x = 0
-	y = 0
+# display
+dims = 480, 160
+screen = pg.display.set_mode(dims)
+
+# game objects
+player = create_player()
+floors = create_floors()
+
+# game loop
+game_status = 1  # 0 for game over, 1 for play
+jump_start = 0
+jump_time = 0
+while game_status:
+
+	game_status, jump = process_input()
 	
-	def __init__(self, x, y, width, height):
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-		
-# the starting gun is loaded
-def play():
-	screen_width = 1000
-	screen_height = 420
-	red = (255,0,0)
-	blue = (0,0,255)
-	white = (255,255,255)
-	player_x = 90
-	player_y = 100
-	player_width = 10
-	player_height = 40
-	all_floors = []
+	player, jump_start, jump_time = move_player(player, jump, jump_start, jump_time)
+	floors = move_floors(floors)
 
-	player_image = pg.image.load('player.png')
-	background_image = pg.image.load('bg.png')
-
-	screen = pg.display.set_mode((screen_width, screen_height))
-	clock = pg.time.Clock()
-
-	while True:
-		#fps
-		clock.tick(48)
-		
-		# jump
-		key = pg.key.get_pressed()
-		if key[pg.K_SPACE]:
-			player_y -= 5
-
-		# gravity
-		collide_x = False;
-		collide_y = False;
-		for floor in all_floors:
-			if (floor.x < player_x < floor.x + floor.width):
-				if player_y < floor.y - player_height:
-					collide_y = False;
-				if player_y >= floor.y - player_height:
-					collide_y = True;
-			if (player_y > floor.y - player_height) and (player_x + player_width < floor.x):
-				player_x
-		if not collide_y:
-			player_y += 3
-			
-		# floor logic	
-		if all_floors == []:
-			all_floors.append(Floor(0, 340, 805, 300))
-		else:
-			for floor in all_floors:
-				if floor.x + floor.width == 800:
-					all_floors.append(Floor(1000, rd.randint(200,400), 805, 400))
-				if floor.x + floor.width <= 0:
-					all_floors.remove(floor)
-
-		# draw background, floors, and player
-		screen.blit(background_image, (0,0))
-		pg.draw.rect(screen, blue, (player_x, player_y, player_width, player_height))
-		for floor in all_floors:
-			floor.x -= 5
-			pg.draw.rect(screen, white, (floor.x, floor.y, floor.width, floor.height))
-		
-		# update
-		pg.display.update()
-		
-		# quit
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				return
-			if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-				return 
-		if player_y == screen_height:
-			return
-
-play()
-	
+	draw_everything(screen, player, floors)
+    
+	clock.tick(50)  # or sleep(.02) to have the loop pg-independent
