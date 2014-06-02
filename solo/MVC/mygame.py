f2bb02cdb043b803f75bb7871384ac03867f7313
Sahand Nayebaziz <snayebaz@uci.edu>
2014-06-02 10:23:47 -0700

    countdown working

--- solo/MVC/mygame.py
+++ solo/MVC/mygame.py
@@ -27,7 +27,7 @@ class Model():
 		self.player = {'x': 60, 'y': 30, 'width': 7, 'height': 30}
 		self.floors = {0: (120, 900)}
 		self.motion = {'counter': 25, 'vel': 3, 'acc': 0.3}
-		self.dash = {'game_status': 'multi', 'multi_status': self.dash['multi_status'], 'multi_timer': 0, 'multi_countdown': 30, 'jumping': False, 'jump_start': 0,  'jump_time': 0, 'falling': False, 'frames_traveled': 0, 'menu_selection': 0, 'high_score': self.dash['high_score'], 'current_score': 0} # "dashboard" containing all stats
+		self.dash = {'game_status': 'multi', 'multi_status': self.dash['multi_status'], 'multi_timer': 0, 'multi_countdown': 1500, 'jumping': False, 'jump_start': 0,  'jump_time': 0, 'falling': False, 'frames_traveled': 0, 'menu_selection': 0, 'high_score': self.dash['high_score'], 'current_score': 0} # "dashboard" containing all stats
 		self.color = (255, 255, 255)
 class View():
 	def __init__(self, model):
@@ -138,33 +138,36 @@ class View():
 				pg.display.update()
 				self.m.dash['multi_timer'] += 1
 			else:
+				self.m.dash['multi_timer'] = 0
 				self.m.dash['multi_status'] = 'ready'
 
 
 
 		if self.m.dash['multi_status'] == 'ready':
-			# am I dead?
-			if self.m.player['x'] + self.m.player['width'] < 0 or self.m.player['y'] > self.dims['y']:
-				self.m.reset_menu()
-			if self.m.player['y'] < 30:
-				self.camera['y'] = self.m.player['y'] - 30
-			screen = self.screen
-			screen.fill((32, 32, 32))
-			pg.draw.rect(screen, (100, 100, 100), (self.m.player['x'], self.m.player['y'] - self.camera['y'], self.m.player['width'], self.m.player['height']))
-			for key in self.m.floors:
-				pg.draw.rect(screen, (50, 50, 50), (key, self.m.floors[key][0] - self.camera['y'], self.m.floors[key][1], 600))
-			if self.m.dash['menu_selection'] == 0:
-				colors = ((102, 202, 204), (120, 120, 120))
-			else:
-				colors = ((120, 120, 120), (102, 202, 204))
-			if pg.font:
-			    big_font = pg.font.Font('range.ttf', 26)
-			    title = big_font.render("New game starting in... ", 1, (255, 255, 255))
-			    screen.blit(title, (130, 40))
-			    if self.m.dash['high_score'] != 0:
-			    	score = little_font.render(("your best run... " + str(self.m.dash['high_score'])), 1, (90,90,90))
-			    	screen.blit(score, (353, 100))
-			pg.display.update()
+			if self.m.dash['multi_countdown'] != 0:
+				# am I dead?
+				if self.m.player['x'] + self.m.player['width'] < 0 or self.m.player['y'] > self.dims['y']:
+					self.m.reset_menu()
+				if self.m.player['y'] < 30:
+					self.camera['y'] = self.m.player['y'] - 30
+				screen = self.screen
+				screen.fill((32, 32, 32))
+				pg.draw.rect(screen, (100, 100, 100), (self.m.player['x'], self.m.player['y'] - self.camera['y'], self.m.player['width'], self.m.player['height']))
+				for key in self.m.floors:
+					pg.draw.rect(screen, (50, 50, 50), (key, self.m.floors[key][0] - self.camera['y'], self.m.floors[key][1], 600))
+				if self.m.dash['menu_selection'] == 0:
+					colors = ((102, 202, 204), (120, 120, 120))
+				else:
+					colors = ((120, 120, 120), (102, 202, 204))
+				if pg.font:
+				    big_font = pg.font.Font('range.ttf', 19)
+				    title = big_font.render("New game starting in... " + (str(self.m.dash['multi_countdown'] / 50)), 1, (255, 255, 255))
+				    screen.blit(title, (96, 30))
+				    if self.m.dash['high_score'] != 0:
+				    	score = little_font.render(("your best run... " + str(self.m.dash['high_score'])), 1, (90,90,90))
+				    	screen.blit(score, (353, 100))
+				pg.display.update()
+				self.m.dash['multi_countdown'] -= 1
 		
 class Controller():
 
